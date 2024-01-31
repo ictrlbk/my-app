511c00045bb264f19dce3bdbe4c911422af3ae42
ictrl <ictrlbk@gmail.com>
2024-01-31 14:56:49 -0500

    initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = 0
+  const increment = () => { count += 1 }
+  const decrement = () => { count -= 1 }
+</script>
+
+<h1>Counter is {count}</h1>
+<button on:click={increment}>increment</button>
+<button on:click={decrement}>decrement</button>
